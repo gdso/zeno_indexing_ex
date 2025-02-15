8019d997dfccd58f9e4c4bc5103230fe50136514
Gregory de Souza <536112+gdso@users.noreply.github.com>
2025-02-15 01:07:58 -0500

    Adding ex_doc to :dev deps and updating mix.lock

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule ZenoIndexingEx.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       licenses: "MIT", 
-      source_url: "https://github.com/gdso/zeno_indexing_ex"
+      source_url: "https://github.com/gdso/zeno_indexing_ex",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -23,7 +23,8 @@ defmodule ZenoIndexingEx.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:jason, "~> 1.0", only: [:test]}
+      {:jason, "~> 1.0", only: [:test]},
+      {:ex_doc, "~> 0.34", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
